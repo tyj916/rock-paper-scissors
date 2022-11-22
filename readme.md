19f9ccd2c3e272c05008fec320d0124c623669fa
Tan Yi Jun <tanyijun6@gmail.com>
2022-11-23 00:06:40 +0800

    Create rock paper scissor game

---
+++ readme.md
@@ -0,0 +1,15 @@
+# rock-paper-scissors
+
+This is a program that allows user to play rock paper scissors with computer.
+
+My first JavaScript program built from scratch. 
+The aim of this program is to practice my problem solving skills, write out algorithm and psuedo-code, and convert those into a JavaScript program.
+
+So I'm going to write out the algorithm below here:
+    start the game
+    ask user to select rock, paper or scissor
+    get a random selection for computer
+    compare user's selection and computer's selection
+        return who's the winner
+    display who's the winner
+    repeat 5 times
---
+++ script.js
@@ -0,0 +1,98 @@
+function getComputerChoice() {
+    const selections = ['rock', 'paper', 'scissor'];
+    return selections[Math.floor(Math.random()*selections.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Draw!';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissor') {
+            return 'You Win! Scissor beats rock!';
+        }
+        if (computerSelection === 'paper') {
+            return 'You Lose! Paper beats rock!';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissor') {
+            return 'You Lose! Scissor beats paper!';
+        }
+        if (computerSelection === 'rock') {
+            return 'You Win! Scissor beats rock!';
+        }
+    } else if (playerSelection === 'scissor') {
+        if (computerSelection === 'rock') {
+            return 'You Lose! Rock beats scissor!';
+        }
+        if (computerSelection === 'paper') {
+            return 'You Win! Scissor beats paper!';
+        }
+    } else {
+        return 'What?! Try again.';
+    }
+}
+
+function game() {
+    let playerScore = 0, computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i+1}! START!`);
+
+        let playerSelection = prompt("Enter your selection: ").toLowerCase();
+        console.log("Player selection: " + playerSelection);
+
+        let computerSelection = getComputerChoice();
+        console.log("Computer selection: " + computerSelection);
+
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes('Win')) {
+            playerScore += 1;
+        } else if (result.includes('Lose')) {
+            computerScore += 1;
+        } else {
+            console.log("Nobody gets score");
+        }
+
+        console.log(`Scoreboard: 
+                     Player: ${playerScore}
+                     Computer: ${computerScore}`);
+    }
+
+    console.log("Game over!");
+    let winner = getWinner(playerScore, computerScore);
+    
+    if (winner === 'player') {
+        console.log("You win!");
+    } else if (winner === 'computer') {
+        console.log("You lose!");
+    } else {
+        console.log("Draw!");
+    }
+}
+
+function getWinner(playerScore, computerScore) {
+    let winner;
+
+    if (playerScore > computerScore) {
+        winner = 'player';
+    } else if (playerScore < computerScore) {
+        winner = 'computer';
+    } else {
+        winner = 'draw';
+    }
+
+    return ((playerScore > computerScore) ? 'player' :
+            (computerScore > playerScore) ? 'computer':
+            'draw');
+}
+
+let askForGame = confirm("Do you want to play a game?");
+
+while(askForGame) {
+    game();
+    askForGame = confirm("Another game?");
+}
+
+console.log("Thanks for playing!");
