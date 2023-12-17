0a344c8b245c5a1486e962f39f7a3e68a23c9ecb
Tan Yi Jun <tanyijun6@gmail.com>
2023-12-18 00:33:15 +0800

    Introducing RPS program prototype

---
+++ script.js
@@ -0,0 +1,63 @@
+function getComputerChoice() {
+    const randomChoice = Math.ceil(Math.random() * 3);
+    return (randomChoice == 1) ? 'Rock' :
+            (randomChoice == 2) ? 'Paper' : 'Scissors';
+}
+
+function capitalizeSelection(playerSelection) {
+    const firstLetter = playerSelection[0];
+    return firstLetter.toUpperCase() + playerSelection.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'Rock':
+            if (computerSelection == 'Scissors') {
+                return 'You Win! Rock beats Scissors';
+            } else if (computerSelection == 'Paper') {
+                return 'You Lose! Paper beats Rock';
+            } else {
+                return 'No one wins! Both of you select Rock';
+            }
+            break;
+
+        case 'Paper':
+            if (computerSelection == 'Rock') {
+                return 'You Win! Paper beats Rock';
+            } else if (computerSelection == 'Scissors') {
+                return 'You Lose! Scissors beats Paper';
+            } else {
+                return 'No one wins! Both of you select Paper';
+            }
+            break;
+
+        case 'Scissors':
+            if (computerSelection == 'Paper') {
+                return 'You Win! Scissors beats Paper';
+            } else if (computerSelection == 'Rock') {
+                return 'You Lose! Rock beats Scissors';
+            } else {
+                return 'No one wins! Both of you select Scissors';
+            }
+            break;
+    }
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter your selection (Rock/Paper/Scissors", '').toLowerCase();
+    while (!(playerChoice == 'rock' || playerChoice == 'paper' || playerChoice == 'scissors')) {
+        playerChoice = prompt("Please enter Rock, Paper and Scissors only", '').toLowerCase();
+    }
+    return capitalizeSelection(playerChoice);
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+    }
+}
+
+game();
